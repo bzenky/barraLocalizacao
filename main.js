a35e5481e4030bfc806bb6df017b48ca6b580620
Zenky <bzenky@gmail.com>
2021-08-25 11:59:06 -0300

    busca por CEP

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 //header - geo
-const continuar = document.querySelector('#cont')
 const alterar = document.querySelector('#alterar')
 const local = document.querySelector('.geolocation')
 const header = document.querySelector('.local')
+const modal = document.querySelector('.modal')
+const fechar = document.querySelector('#fechar')
+const cep = document.querySelector('.cep')
+const buscar = document.querySelector('#buscar')
 
 fetch('https://ipapi.co/json/')
 .then(function(response) {
@@ -14,8 +17,36 @@ fetch('https://ipapi.co/json/')
   alert('Erro!')
 });
 
-continuar.addEventListener('click', () => {
-  header.classList.add('fechar')
+
+// Botão Alterar - Modal
+alterar.addEventListener('click', () => {
+  modal.classList.add('active')
 })
+
+fechar.addEventListener('click', () => {
+  modal.classList.remove('active')
+})
+
+// API CEP
+function buscaCEP() {
+  cepBusca = cep.value
+
+  fetch('https://viacep.com.br/ws/' + cepBusca + '/json/')
+    .then(function(response) {
+      response.json().then(jsonData => {
+        local.innerText = jsonData.localidade
+      });
+    })
+    .catch(function(error) {
+      alert('Erro!')
+    });
+    
+  modal.classList.remove('active')
+  cep.value = ''
+}
+
+buscar.addEventListener('click', buscaCEP)
+
+
 
 
